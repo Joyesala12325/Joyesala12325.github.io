09406aa8e3552d656076f243bc8952c36f23fedd
Joyesala12325 <116412884+Joyesala12325@users.noreply.github.com>
2023-10-19 13:46:30 -0500

    Initial commit

---
+++ js/lightbox.js
@@ -0,0 +1,25 @@
+const imagenes = document.querySelectorAll('.img-galeria');
+const imagenesLight = document.querySelector('.agregar-imagen');
+const contenedorLight = document.querySelector('.imagen-light');
+const hamburguer1 = document.querySelector('.hamburguer');
+
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click', ()=>{
+        aparecerImagen(imagen.getAttribute('src'))
+    })
+})
+
+contenedorLight.addEventListener('click', (e)=>{
+    if(e.target !== imagenesLight){
+        contenedorLight.classList.toggle('show')
+        imagenesLight.classList.toggle('showImage')
+        hamburguer1.style.opacity = '1'
+    }
+})
+
+const aparecerImagen = (imagen)=>{
+    imagenesLight.src = imagen;
+    contenedorLight.classList.toggle('show')
+    imagenesLight.classList.toggle('showImage')
+    hamburguer1.style.opacity = '0'
+}
